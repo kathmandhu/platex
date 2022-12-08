bd5404b0293a9857d37f23d314417abebabe31cd
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-12-08 17:10:29 +0545

    Update axios.ts

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -4,7 +4,7 @@ import { API_URL } from "./constants";
 const instance = axios.create({
   baseURL: API_URL,
   params: {
-    api_key: process.env.TMDB,
+    api_key: "76193ed2bcf26de2dc230e6c660a549e",
   },
 });
 
