51ec768746f288a6c70b7ad1fdb418da92980442
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-12-08 18:43:21 +0545

    feat: fixed

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -2,38 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const app = initializeApp({
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId,
-// });
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDo3jA6gJwcTPpeOkaQ12nPorLXGZywBc8",
-  authDomain: "freeies.firebaseapp.com",
-  projectId: "freeies",
-  storageBucket: "freeies.appspot.com",
-  messagingSenderId: "1058228808320",
-  appId: "1:1058228808320:web:ca8a004f6472d502df5465",
-  measurementId: "G-VKQ6C6D5L9",
+  apiKey: "AIzaSyBnzYjLYJGN-sIY4ULGFnRywlMLsMSXH4I",
+  authDomain: "platex-webseries.firebaseapp.com",
+  projectId: "platex-webseries",
+  storageBucket: "platex-webseries.appspot.com",
+  messagingSenderId: "1040708366179",
+  appId: "1:1040708366179:web:2a22dc7a46945d464bad56",
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC5V0Pm-YJV9h0kyX9aBgrKrs3_J3R2Foo",
-//   authDomain: "pvt-comments.firebaseapp.com",
-//   databaseURL: "https://pvt-comments-default-rtdb.firebaseio.com",
-//   projectId: "pvt-comments",
-//   storageBucket: "pvt-comments.appspot.com",
-//   messagingSenderId: "700353567702",
-//   appId: "1:700353567702:web:f36bbbae9c78ac1e3ea277",
-// };
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-// export const db = initializeFirestore(app, {
-//   experimentalForceLongPolling: true,
-// });
 export const auth = getAuth(app);
