e697fb0571047da1be5bec6bbfdb5a6ec4b935f3
Biraj Rai <70630924+birajrai@users.noreply.github.com>
2022-12-10 11:17:15 +0545

    feat: uhm

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnzYjLYJGN-sIY4ULGFnRywlMLsMSXH4I",
-  authDomain: "platex-webseries.firebaseapp.com",
-  projectId: "platex-webseries",
-  storageBucket: "platex-webseries.appspot.com",
-  messagingSenderId: "1040708366179",
-  appId: "1:1040708366179:web:2a22dc7a46945d464bad56",
+  apiKey: "AIzaSyAO10PW9n1uQpnJK5ZVkOh3p68ebrj6GDU",
+  authDomain: "platex-web.firebaseapp.com",
+  projectId: "platex-web",
+  storageBucket: "platex-web.appspot.com",
+  messagingSenderId: "86430679528",
+  appId: "1:86430679528:web:25976e21f3b178f758645d"
 };
 
 const app = initializeApp(firebaseConfig);
